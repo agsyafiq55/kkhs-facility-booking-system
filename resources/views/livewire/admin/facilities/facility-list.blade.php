94581debc86919dd7627b4d62d504d0e6143a9d5
agsyafiq55 <agsyafiq55@gmail.com>
2025-05-19 13:29:42 +0800

    Change button variant in facility admin list

--- resources/views/livewire/admin/facilities/facility-list.blade.php
+++ resources/views/livewire/admin/facilities/facility-list.blade.php
@@ -98,8 +98,7 @@
 
                             <!-- Action Buttons -->
                             <div class="flex justify-end space-x-2 mt-4">
-                                <flux:button variant="ghost" href="{{ route('admin.facilities.show', $facility) }}" wire:navigate
-                                    class="text-sm">
+                                <flux:button href="{{ route('admin.facilities.show', $facility) }}" wire:navigate>
                                     <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1 inline-block" viewBox="0 0 20 20" fill="currentColor">
                                         <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
                                         <path fill-rule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clip-rule="evenodd" />
@@ -107,8 +106,7 @@
                                     View
                                 </flux:button>
                                 
-                                <flux:button variant="ghost" href="{{ route('admin.facilities.edit', $facility) }}" wire:navigate
-                                    class="text-sm bg-transparent hover:bg-blue-50 dark:hover:bg-blue-900 text-blue-600 dark:text-blue-400">
+                                <flux:button href="{{ route('admin.facilities.edit', $facility) }}" wire:navigate>
                                     <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1 inline-block" viewBox="0 0 20 20" fill="currentColor">
                                         <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
                                     </svg>
